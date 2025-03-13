98f41c5ea01386da0deab5a935d2c1feb869de9c
Miguel <miguelmonsalve355@gmail.com>
2025-03-13 18:13:18 -0500

    Proyecto terminado

---
+++ ServicioREST/ServicioREST/Clases/Computadores.cs
@@ -0,0 +1,86 @@
+﻿using ServicioREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace ServicioREST.Clases
+{
+    public class Computadores
+    {
+        private VentaComputadoresEntities dbsuper = new VentaComputadoresEntities();
+
+        public Computador computador { get; set; }
+
+        public string Ingresar()
+        {
+            try
+            {
+                dbsuper.Computador.Add(computador);
+                dbsuper.SaveChanges();
+                return "Se ha ingresado el computador éxitosamente";
+
+            }
+            catch (Exception ex)
+            {
+                return "El error ocurrido es: " + ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            Computador comp = Consultar(computador.IdComputador);
+            try
+            {
+                if (comp == null)
+                {
+                    return "El computador no se puede actualizar porque no existe";
+                }
+                else
+                {
+                    dbsuper.Computador.AddOrUpdate(computador);
+                    dbsuper.SaveChanges();
+                    return "El computador ha sido actualizado con éxito";
+                }
+            } catch (Exception ex) {
+
+                return "No se puede actualizar debido al error: " + ex.Message;
+            }
+        }
+
+        public Computador Consultar(int id)
+        {
+            return dbsuper.Computador.FirstOrDefault(c => c.IdComputador == id);
+        }
+
+        public string Eliminar()
+        {
+            Computador comp = Consultar(computador.IdComputador);
+
+            try
+            {
+                if(comp == null)
+                {
+                   return "El computador no se puede eliminar porque no existe";
+                }
+                else
+                {
+                    dbsuper.Computador.Remove(comp);
+                    dbsuper.SaveChanges();
+                    return "El computador se eliminó correctamente";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar debido al error: " + ex.Message;
+            }
+        }
+
+        public List<Computador> ConsultarTodos()
+        {
+            return dbsuper.Computador.ToList();
+        }
+    }
+}
---
+++ ServicioREST/ServicioREST/Controllers/ComputadoresController.cs
@@ -0,0 +1,69 @@
+﻿using ServicioREST.Clases;
+using ServicioREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Servicios_Jue.Controllers
+{
+    [RoutePrefix("api/Computadores")]
+    public class ComputadoresController : ApiController
+    {
+     
+        [HttpGet] 
+        [Route("ConsultarTodos")] 
+        public List<Computador> ConsultarTodos()
+        {
+           
+            Computadores computador = new Computadores();
+            return computador.ConsultarTodos();
+        }
+        
+        [HttpGet]
+        [Route("ConsultarXID")]
+        public Computador ConsultarXID(int id)
+        {
+
+            Computadores computador = new Computadores();
+            return computador.Consultar(id); 
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Computador computador)
+        {
+  
+            Computadores COMPUTADOR = new Computadores();
+
+            COMPUTADOR.computador = computador;
+           
+            return COMPUTADOR.Ingresar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Computador computador)
+        {
+            Computadores COMPUTADOR = new Computadores();
+
+            COMPUTADOR.computador = computador;
+
+            return COMPUTADOR.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Computador computador)
+        {
+            Computadores COMPUTADOR = new Computadores();
+
+            COMPUTADOR.computador = computador;
+
+            return COMPUTADOR.Eliminar();
+        }
+
+    }
+}
